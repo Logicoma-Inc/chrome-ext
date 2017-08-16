c9fd1059a63cd67b559451332ed7048a406b5a80
Åℵ┬ℏÔñÝ ℉⅍§ë‡ <fassetar@users.noreply.github.com>
2017-08-16 14:02:58 -0400

    Add files via upload

---
+++ popup.js
@@ -0,0 +1,214 @@
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Search the bookmarks when entering the search keyword.
+$(function() {
+  $('#search').change(function() {
+     $('#bookmarks').empty();
+     dumpBookmarks($('#search').val());
+  });
+});
+// Traverse the bookmark tree, and print the folder and nodes.
+function dumpBookmarks(query) {
+  var bookmarkTreeNodes = chrome.bookmarks.getTree(
+    function(bookmarkTreeNodes) {
+      $('#bookmarks').append(dumpTreeNodes(bookmarkTreeNodes, query));
+    });
+}
+function dumpTreeNodes(bookmarkNodes, query) {
+  var list = $('<ul>');
+  var i;
+  for (i = 0; i < bookmarkNodes.length; i++) {
+    list.append(dumpNode(bookmarkNodes[i], query));
+  }
+  return list;
+}
+function dumpNode(bookmarkNode, query) {
+  if (bookmarkNode.title) {
+    if (query && !bookmarkNode.children) {
+      if (String(bookmarkNode.title).indexOf(query) == -1) {
+        return $('<span></span>');
+      }
+    }
+    var anchor = $('<a>');
+    anchor.attr('href', bookmarkNode.url);
+    anchor.text(bookmarkNode.title);
+    /*
+     * When clicking on a bookmark in the extension, a new tab is fired with
+     * the bookmark url.
+     */
+    anchor.click(function() {
+      chrome.tabs.create({url: bookmarkNode.url});
+    });
+    var span = $('<span>');
+    var options = bookmarkNode.children ?
+      $('<span>[<a href="#" id="addlink">Add</a>]</span>') :
+      $('<span>[<a id="editlink" href="#">Edit</a> <a id="deletelink" ' +
+        'href="#">Delete</a>]</span>');
+    var edit = bookmarkNode.children ? $('<table><tr><td>Name</td><td>' +
+      '<input id="title"></td></tr><tr><td>URL</td><td><input id="url">' +
+      '</td></tr></table>') : $('<input>');
+    // Show add and edit links when hover over.
+        span.hover(function() {
+        span.append(options);
+        $('#deletelink').click(function() {
+          $('#deletedialog').empty().dialog({
+                 autoOpen: false,
+                 title: 'Confirm Deletion',
+                 resizable: false,
+                 height: 140,
+                 modal: true,
+                 overlay: {
+                   backgroundColor: '#000',
+                   opacity: 0.5
+                 },
+                 buttons: {
+                   'Yes, Delete It!': function() {
+                      chrome.bookmarks.remove(String(bookmarkNode.id));
+                      span.parent().remove();
+                      $(this).dialog('destroy');
+                    },
+                    Cancel: function() {
+                      $(this).dialog('destroy');
+                    }
+                 }
+               }).dialog('open');
+         });
+        $('#addlink').click(function() {
+          $('#adddialog').empty().append(edit).dialog({autoOpen: false,
+            closeOnEscape: true, title: 'Add New Bookmark', modal: true,
+            buttons: {
+            'Add' : function() {
+               chrome.bookmarks.create({parentId: bookmarkNode.id,
+                 title: $('#title').val(), url: $('#url').val()});
+               $('#bookmarks').empty();
+               $(this).dialog('destroy');
+               window.dumpBookmarks();
+             },
+            'Cancel': function() {
+               $(this).dialog('destroy');
+            }
+          }}).dialog('open');
+        });
+        $('#editlink').click(function() {
+         edit.val(anchor.text());
+         $('#editdialog').empty().append(edit).dialog({autoOpen: false,
+           closeOnEscape: true, title: 'Edit Title', modal: true,
+           show: 'slide', buttons: {
+              'Save': function() {
+                 chrome.bookmarks.update(String(bookmarkNode.id), {
+                   title: edit.val()
+                 });
+                 anchor.text(edit.val());
+                 options.show();
+                 $(this).dialog('destroy');
+              },
+             'Cancel': function() {
+                 $(this).dialog('destroy');
+             }
+         }}).dialog('open');
+        });
+        options.fadeIn();
+      },
+      // unhover
+      function() {
+        options.remove();
+      }).append(anchor);
+  }
+  var li = $(bookmarkNode.title ? '<li>' : '<div>').append(span);
+  if (bookmarkNode.children && bookmarkNode.children.length > 0) {
+    li.append(dumpTreeNodes(bookmarkNode.children, query));
+  }
+  return li;
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+  dumpBookmarks();
+  chrome.bookmarks.getTree(
+    function(bookmarkTreeNodes) {
+        var links = [];
+        $.each(bookmarkTreeNodes[0].children[0].children, function(key, value){
+             links.push({
+                source: value.title,
+                target: "Bookmarks",
+                type: 'link'
+             });
+        });         
+        console.log(links.length);           
+        var nodes = {};
+
+        // Compute the distinct nodes from the links.
+        links.forEach(function(link) {
+        link.source = nodes[link.source] || (nodes[link.source] = {name: link.source});
+        link.target = nodes[link.target] || (nodes[link.target] = {name: link.target});
+        });
+
+        var width = 400,
+            height = 400;
+
+        var force = d3.layout.force()
+            .nodes(d3.values(nodes))
+            .links(links)
+            .size([width, height])
+            .linkDistance(60)
+            .charge(-300)
+            .on("tick", tick)
+            .start();
+
+        var svg = d3.select("body").append("svg")
+            .attr("width", width)
+            .attr("height", height);
+
+        // Per-type markers, as they don't inherit styles.
+        svg.append("defs").selectAll("marker")
+            .data(["suit", "licensing", "resolved"])
+        .enter().append("marker")
+            .attr("id", function(d) { return d; })
+            .attr("viewBox", "0 -5 10 10")
+            .attr("refX", 15)
+            .attr("refY", -1.5)
+            .attr("markerWidth", 6)
+            .attr("markerHeight", 6)
+            .attr("orient", "auto")
+        .append("path")
+            .attr("d", "M0,-5L10,0L0,5");
+
+        var path = svg.append("g").selectAll("path")
+            .data(force.links())
+        .enter().append("path")
+            .attr("class", function(d) { return "link " + d.type; })
+            .attr("marker-end", function(d) { return "url(#" + d.type + ")"; });
+
+        var circle = svg.append("g").selectAll("circle")
+            .data(force.nodes())
+        .enter().append("circle")
+            .attr("r", 6)
+            .call(force.drag);
+
+        var text = svg.append("g").selectAll("text")
+            .data(force.nodes())
+        .enter().append("text")
+            .attr("x", 8)
+            .attr("y", ".31em")
+            .text(function(d) { return d.name; });
+
+        // Use elliptical arc path segments to doubly-encode directionality.
+        function tick() {
+        path.attr("d", linkArc);
+        circle.attr("transform", transform);
+        text.attr("transform", transform);
+        }
+
+        function linkArc(d) {
+        var dx = d.target.x - d.source.x,
+            dy = d.target.y - d.source.y,
+            dr = Math.sqrt(dx * dx + dy * dy);
+        return "M" + d.source.x + "," + d.source.y + "A" + dr + "," + dr + " 0 0,1 " + d.target.x + "," + d.target.y;
+        }
+
+        function transform(d) {
+        return "translate(" + d.x + "," + d.y + ")";
+        }   
+    });    
+});
